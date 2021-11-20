15ed1bb9eea5b5fa1d2b64c988a9d56d72ec310b
vinast <dvvinas2@gmail.com>
2021-11-20 16:15:25 +0700

    testing pertama

---
+++ index.js
@@ -0,0 +1,5 @@
+const nama = "Vinast"; // const cuma bisa di pake sekali
+let hobi = "Futsal";
+hobi = "volly"; //let bisa di pake lebih dari satu
+
+console.log(`${nama} ${hobi}`); //buat cek atau tracking
